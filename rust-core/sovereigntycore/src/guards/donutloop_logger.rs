5cb0be7eb70c48c8e45cf35e0b7f3d201223aaf5
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-06 15:22:51 -0700

    Create donutloop_logger.rs

---
+++ rust-core/sovereigntycore/src/guards/donutloop_logger.rs
@@ -0,0 +1,7 @@
+pub struct DonutloopLogger;
+
+impl DonutloopLogger {
+    pub fn log_event(&self, _kind: &str, _msg: &str) {
+        // In a full implementation, append to .donutloop.aln.
+    }
+}
