2bbb2601d698b9b39e308dff991b015fe5727006
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2026-02-06 15:25:31 -0700

    Create init.py

---
+++ jupyter-client/neuropc_jupyter_client/init.py
@@ -0,0 +1,13 @@
+from .config import NeuroPCConfig
+from .neurofs import NeuroFSIndex
+from .shards_client import ShardsClient
+from .evolution_proposals import EvolutionProposalRecord
+from .sovereignty_client import SovereigntyClient
+
+__all__ = [
+    "NeuroPCConfig",
+    "NeuroFSIndex",
+    "ShardsClient",
+    "EvolutionProposalRecord",
+    "SovereigntyClient",
+]
